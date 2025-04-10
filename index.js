92a5dc64e1886183de4d2c825cc82914632a452a
KubaLubiMaslo <czemek.jakub@gmail.com>
2025-04-10 16:20:51 +0200

    try to see if save file works + test png return

--- index.js
+++ index.js
@@ -2,16 +2,80 @@ const http = require('http');
 const path = require('path');
 const fs = require('fs');
 
+const { sleep } = require('./modules');
+
+let contentmatchcheck = false;
+    
+(async () => {
+    console.log("Starting...");
+    
+    await sleep(1000);
+    
+    const filename = './special.txt'
+    const dataToWrite = 'test';
+    
+    fs.writeFileSync(filename, dataToWrite);
+    
+    const dataRead = fs.readFileSync(filename, 'utf8');
+    
+    if (dataRead === dataToWrite) {
+      console.log('Success: Content matches!');
+      contentmatchcheck = true;
+    } else {
+      console.error('Error: Content does not match.');
+      contentmatchcheck = false;
+    }
+    
+    fs.unlinkSync(filename);
+})();
+
 http.createServer(function(req, res) {
-    if (!(req.url == "/favicon.ico")) console.log("req");
-    fs.readFile('./index.html', function(err, data) {
+    if (req.url == "/favicon.ico") {
+        fs.readFile('./kj.png', function(err, data) {
+            if (err) { 
+                res.writeHead(400);
+                res.end(); 
+                return; 
+            }
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.end(data);
+            return;
+        });
+        return;
+    } else console.log("req");
+
+    if (req.url == "/upload") {
+        fs.readFile('./upload.html', function(err, data) {
+            if (err) { 
+                res.writeHead(400);
+                res.end(); 
+                return; 
+            }
+    
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data);
+            return;
+        });
+        return;
+    }
+    if (req.url == "/check") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        
+        if (contentmatchcheck) res.end('true');
+        else res.end('false');
+
+        return;
+    }
+
+    
+    fs.readFile('./kj.png', function(err, data) {
         if (err) { 
             res.writeHead(400);
             res.end(); 
             return; 
         }
-
-        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.writeHead(200, {'Content-Type': 'image/png'});
         res.end(data);
+        return;
     });
 }).listen(61021);
