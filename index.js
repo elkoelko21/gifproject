466487c8724e2cd178c578ceadf0bdd5fbdc9864
KubaLubiMaslo <czemek.jakub@gmail.com>
2025-04-10 18:19:37 +0200

    try to make uploadable files

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const Busboy = require('busboy');
 
 const { sleep } = require('./modules');
-
-fs.readdirSync('../../').forEach(file => {
-    console.log(file);
-});
 
 http.createServer(function(req, res) {
     if (req.url == "/favicon.ico") {
@@ -38,15 +35,44 @@ http.createServer(function(req, res) {
         return;
     }
 
-    
-    fs.readFile('./kj.png', function(err, data) {
-        if (err) { 
-            res.writeHead(400);
-            res.end(); 
-            return; 
-        }
-        res.writeHead(200, {'Content-Type': 'image/png'});
-        res.end(data);
+    if (req.url == "/uploadimg" && req.method === "POST") {
+        const busboy = new Busboy({ headers: req.headers });
+
+        busboy.on('file', function(fieldname, file, filename) {
+            file.pipe(fs.createWriteStream('image.png'));
+        });
+
+        busboy.on('finish', function () {
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('File uploaded successfully.');
+            console.log();
+        });
+
+        req.pipe(busboy);
         return;
-    });
+    }
+
+    if (fs.existsSync('./image.png')) {
+        fs.readFile('./image.png', function(err, data) {
+            if (err) { 
+                res.writeHead(400);
+                res.end(); 
+                return; 
+            }
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.end(data);
+            return;
+        });
+    } else {
+        fs.readFile('./kj.png', function(err, data) {
+            if (err) { 
+                res.writeHead(400);
+                res.end(); 
+                return; 
+            }
+            res.writeHead(200, {'Content-Type': 'image/png'});
+            res.end(data);
+            return;
+        });
+    }
 }).listen(61021);
