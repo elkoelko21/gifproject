ebc73856852cb27e5af6615682ff46828e04b82b
KubaLubiMaslo <czemek.jakub@gmail.com>
2025-04-10 17:05:37 +0200

    delete file create test and create file scan test

--- index.js
+++ index.js
@@ -4,30 +4,9 @@ const fs = require('fs');
 
 const { sleep } = require('./modules');
 
-let contentmatchcheck = false;
-    
-(async () => {
-    console.log("Starting...");
-    
-    await sleep(1000);
-    
-    const filename = './special.txt'
-    const dataToWrite = 'test';
-    
-    fs.writeFileSync(filename, dataToWrite);
-    
-    const dataRead = fs.readFileSync(filename, 'utf8');
-    
-    if (dataRead === dataToWrite) {
-      console.log('Success: Content matches!');
-      contentmatchcheck = true;
-    } else {
-      console.error('Error: Content does not match.');
-      contentmatchcheck = false;
-    }
-    
-    fs.unlinkSync(filename);
-})();
+fs.readdirSync('./').forEach(file => {
+    console.log(file);
+});
 
 http.createServer(function(req, res) {
     if (req.url == "/favicon.ico") {
@@ -56,14 +35,6 @@ http.createServer(function(req, res) {
             res.end(data);
             return;
         });
-        return;
-    }
-    if (req.url == "/check") {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        
-        if (contentmatchcheck) res.end('true');
-        else res.end('false');
-
         return;
     }
 
